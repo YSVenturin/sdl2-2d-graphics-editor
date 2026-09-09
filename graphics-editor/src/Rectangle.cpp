cd7d145ab017175d5c32c60c37a6f1263a33df8c
ViniciusG1 <vgmineira@ucs.br>
2026-09-09 18:40:54 -0300

    feat: implement rectangle rotation and selection

--- graphics-editor/src/Rectangle.cpp
+++ graphics-editor/src/Rectangle.cpp
@@ -1,4 +1,5 @@
 #include "Rectangle.h"
+#include <algorithm>
 
 Rectangle::Rectangle() : Shape() {
 }
@@ -21,6 +22,13 @@ void Rectangle::draw() {
     Line(topRight, bottomRight, color).draw();
     Line(bottomRight, bottomLeft, color).draw();
     Line(bottomLeft, topLeft, color).draw();
+
+    if (this->selected) {
+        Shape::drawHandle(topLeft);
+        Shape::drawHandle(topRight);
+        Shape::drawHandle(bottomLeft);
+        Shape::drawHandle(bottomRight);
+    }
 }
 
 void Rectangle::translate(double tx, double ty) {
@@ -35,4 +43,20 @@ void Rectangle::scale(Point origin, double ex, double ey) {
     topRight.scale(origin, ex, ey);
     bottomLeft.scale(origin, ex, ey);
     bottomRight.scale(origin, ex, ey);
+}
+
+void Rectangle::rotate(Point origin, double angleDegrees) {
+    topLeft.rotate(origin, angleDegrees);
+    topRight.rotate(origin, angleDegrees);
+    bottomLeft.rotate(origin, angleDegrees);
+    bottomRight.rotate(origin, angleDegrees);
+}
+
+double Rectangle::distanceTo(Point p) {
+    double d1 = Shape::distancePointToSegment(p, topLeft, topRight);
+    double d2 = Shape::distancePointToSegment(p, topRight, bottomRight);
+    double d3 = Shape::distancePointToSegment(p, bottomRight, bottomLeft);
+    double d4 = Shape::distancePointToSegment(p, bottomLeft, topLeft);
+
+    return std::min(std::min(d1, d2), std::min(d3, d4));
 }
