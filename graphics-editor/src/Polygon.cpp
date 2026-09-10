0e8dd0b4529fa41186d1fb06f0e1a8bc02464b33
ViniciusG1 <vgmineira@ucs.br>
2026-09-09 23:14:05 -0300

    feat: implement polygon interaction methods

--- graphics-editor/src/Polygon.cpp
+++ graphics-editor/src/Polygon.cpp
@@ -26,6 +26,16 @@ void Polygon::draw() {
     Point p1 = (this->points).back();
     Point p2 = (this->points).front();
     Line(p1, p2, this->color).draw();
+
+    if (this->selected) {
+        for (Point &v : this->points) {
+            Shape::drawHandle(v);
+        }
+    }
+}
+
+void Polygon::addPoint(Point p) {
+    this->points.push_back(p);
 }
 
 void Polygon::translate(double tx, double ty) {
@@ -38,4 +48,28 @@ void Polygon::scale(Point origin, double ex, double ey) {
     for (auto& p : this->points) {
         p.scale(origin, ex, ey);
     }
+}
+
+void Polygon::rotate(Point origin, double angleDegrees) {
+    for (auto& p : this->points) {
+        p.rotate(origin, angleDegrees);
+    }
+}
+
+double Polygon::distanceTo(Point p) {
+    if (this->points.empty()) {
+        return 1e18;
+    }
+
+    double minDist = 1e18;
+
+    for (auto it = this->points.begin(); std::next(it) != this->points.end(); it++) {
+        double d = Shape::distancePointToSegment(p, *it, *std::next(it));
+        if (d < minDist) minDist = d;
+    }
+
+    double dClose = Shape::distancePointToSegment(p, this->points.back(), this->points.front());
+    if (dClose < minDist) minDist = dClose;
+
+    return minDist;
 }
